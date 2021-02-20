00e90fd35507ee7bcdec339695ee238f3546dcc2
jessny <jessnyjoseph@gmail.com>
2021-02-20 15:46:41 -0600

    added app.js to root of src dir

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,17 +1,25 @@
-import React, {Component} from "react";
-import Footer from "./components/Footer";
-import Jumbotron from "./components/Jumbotron";
-import Navbar from "./components/Navbar";
+import React, { Component } from "react";
+// import Footer from "./components/Footer";
+// import Jumbotron from "./components/Jumbotron";
+// import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
-import simpsons from "./simpsons.json";
+// import simpsons from "./simpsons.json";
 
 class App extends Component {
   state = {
 
   }
-};
 
 
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    );
+  }
+
+}
 
 
 export default App;
