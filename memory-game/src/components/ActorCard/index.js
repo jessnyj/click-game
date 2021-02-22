378e9c3af93f54bc30c56651260ae01cbddc3e48
jessny <jessnyjoseph@gmail.com>
2021-02-21 23:05:28 -0600

    added container and styling to thumbnail img

--- memory-game/src/components/ActorCard/index.js
+++ memory-game/src/components/ActorCard/index.js
@@ -3,9 +3,10 @@ import "./style.css";
 
 function ActorCard(props) {
     return (
-        // <div className="card-deck">
+        <div className="container">
         <div>
             <img src={props.image} alt={props.name} className="img-thumbnail"></img>
+        </div>
         </div>
     );
 }
