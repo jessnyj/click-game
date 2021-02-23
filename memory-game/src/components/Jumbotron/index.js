9cdfabead07bfefea874c1cfc557efda053c6cea
jessny <jessnyjoseph@gmail.com>
2021-02-22 18:03:28 -0600

    added styling to jumbotron

--- memory-game/src/components/Jumbotron/index.js
+++ memory-game/src/components/Jumbotron/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Fluid jumbotron</h1>
-                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <h1 className="display-4">The Simpsons Game</h1>
+                <p className="lead">Click on an image to earn points, but don't select an image more than once!</p>
             </div>
         </div>
     )
