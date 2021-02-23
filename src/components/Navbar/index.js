6e8a2f62ca970e9d6ca1e41797cfc920833ff952
jessny <jessnyjoseph@gmail.com>
2021-02-22 22:30:55 -0600

    moved everything to root dir

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Navbar(props) {
     return (
             <nav className="navbar sticky-top">
-                <div className="navbar-brand"><a class="link" href="/">The Simpsons Game</a></div>
+                <div className="navbar-brand"><a className="link" href="/">The Simpsons Game</a></div>
                 <div className="navbar-text">{props.rightWrong}</div>
                 <div className="navbar-text">Score: {props.score} | Top Score: {props.topScore}</div>
             </nav>
